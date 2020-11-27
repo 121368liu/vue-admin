622b5fce8c4b8c86fec27316344ef14156efe786
xuanxuan liu <1552248173@qq.com>
2020-11-27 13:20:07 +0800

    构建项目、配置全局样式、路由、elementUi

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    //路由重定向
+    redirect:'login'
+  }, {
+    path: "/login",
+    name: "Login",
+    //使用箭头函数将文件指向组件
+    component: ()=>import("../views/Login/index.vue")
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
 ];
 
 const router = new VueRouter({
